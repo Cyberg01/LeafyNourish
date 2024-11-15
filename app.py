af176163672c3a813d5ed3a8785456036e1531ab
Ananta Alwy <ananta.alwy01@gmail.com>
2024-11-15 13:49:15 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+import pickle
+
+model = pickle.load(open('ml_model.pkl', 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+@app.route("/predict", methods=["POST"])
+
+def predict():
+    nitrogen = request.form.get('nitrogen')
+    fosfor = request.form.get('fosfor')
+    kalium = request.form.get('kalium')
+    suhu = request.form.get('suhu')
+    kelembapan = request.form.get('kelembapan')
+    ph = request.form.get('ph')
+
+    input_query = np.array([[nitrogen, fosfor, kalium, suhu, kelembapan, ph]])
+
+    recommendation = model.predict(input_query)[0]
+
+    return jsonify({"Plant":str(recommendation)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
